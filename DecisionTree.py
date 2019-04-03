155c0f2c3e651e867954cdde2860fb6ac0ed5977
Ha Quoc Tien <17521122@gm.uit.edu.vn>
2019-04-03 18:39:21 +0700

    Print y_pred and remove roc_auc

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 29 17:25:28 2019
-
-@author: tien
-"""
+# nhập thư viện cần dùng
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
@@ -35,13 +30,12 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, rand
 dt = DecisionTreeClassifier()
 dt.fit(x_train, y_train)
 y_pred = Y_ohe.inverse_transform(dt.predict(x_test))
-
+print('Prediction result for first 10 test examples:')
+print(y_pred[:10])
 # điều chỉnh model theo độ cao cây tối đa và so sánh độ chính xác bằng 5-fold cv
 depths = [i for i in range(1, 5)]
 trainingScore5fcv = []
 testingScore5fcv = []
-trainingScoreRocAuc = []
-testingScoreRocAuc = []
 
 for i in depths:
     dtc = DecisionTreeClassifier(max_depth=i).fit(x_train, y_train.ravel())
@@ -49,16 +43,10 @@ for i in depths:
     trainingScore5fcv.append(score)
     score = np.mean(cross_val_score(dtc, x_test, y_test.ravel(), cv = 5))
     testingScore5fcv.append(score)
-    roc_auc = roc_auc_score(y_train, dtc.predict(x_train))
-    trainingScoreRocAuc.append(roc_auc)
-    roc_auc = roc_auc_score(y_test, dtc.predict(x_test))
-    testingScoreRocAuc.append(roc_auc)
 
 # vẽ biểu đồ, so sánh độ chính xác giữa những số neighbors và giữa trên training set vs test set
 plt.plot(depths, trainingScore5fcv, 'b', label = '5-fold CV score on training set')
 plt.plot(depths, testingScore5fcv, 'r', label = '5-fold CV score on testing set')
-plt.plot(depths, trainingScoreRocAuc, 'g', label = 'roc auc score on training set')
-plt.plot(depths, testingScoreRocAuc, 'magenta', label = 'roc auc score on testing set')
 plt.legend()
 plt.ylabel('score')
 plt.xlabel('tree depth')
--- KNN.py
+++ KNN.py
@@ -1,14 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+# nhập các thư viện
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import roc_auc_score
 from matplotlib import pyplot as plt
 
 # đọc file
@@ -19,12 +13,14 @@ f.close()
 X = data[:, :3]
 Y = data[:, 3]
 
+before  = X[:3]
+
 # one hot encode các features và nhãn để công bằng trong việc so sánh các giá
 # trị của các features
 X_ohe = OneHotEncoder(handle_unknown="ignore")
 X_ohe.fit(X)
 X = X_ohe.transform(X)
-
+after = X[:3]
 Y_ohe = LabelBinarizer()
 Y_ohe.fit(Y)
 Y = Y_ohe.transform(Y)
@@ -35,35 +31,27 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, rand
 # model để phân lớp 1-NN => đầu ra
 OneNNClassifier = KNeighborsClassifier(n_neighbors=2, weights = 'distance').fit(x_train, y_train.ravel())
 y_pred_1NN = Y_ohe.inverse_transform(OneNNClassifier.predict(x_test))
+print('Prediction result for first 10 test examples:')
+print(y_pred_1NN[:15])
 
 # xây dựng model KNN với số các neighbors khác nhau để tìm được số tốt nhất từ 3->30
 numberOfNeighbors = [i for i in range(3,30)]
 trainingScore5fcv = []
 testingScore5fcv = []
-trainingScoreRocAuc = []
-testingScoreRocAuc = []
 
 for i in numberOfNeighbors:
-    knnClassifier = KNeighborsClassifier(n_neighbors=i, weights = 'distance').fit(x_train, y_train.ravel())
+    knnClassifier = KNeighborsClassifier(algorithm= 'brute',n_neighbors=i, weights = 'distance').fit(x_train, y_train.ravel())
     score = np.mean(cross_val_score(knnClassifier, x_train, y_train.ravel(), cv = 5))
     trainingScore5fcv.append(score)
     score = np.mean(cross_val_score(knnClassifier, x_test, y_test.ravel(), cv = 5))
     testingScore5fcv.append(score)
-    roc_auc = roc_auc_score(y_train, knnClassifier.predict(x_train))
-    trainingScoreRocAuc.append(roc_auc)
-    roc_auc = roc_auc_score(y_test, knnClassifier.predict(x_test))
-    testingScoreRocAuc.append(roc_auc)
-
 
 # vẽ biểu đồ, so sánh độ chính xác giữa những số neighbors và giữa trên training set vs test set
 plt.plot(numberOfNeighbors, trainingScore5fcv, 'b', label = '5-fold CV score on training set')
 plt.plot(numberOfNeighbors, testingScore5fcv, 'r', label = '5-fold CV score on testing set')
-plt.plot(numberOfNeighbors, trainingScoreRocAuc, 'g', label = 'roc auc score on training set')
-plt.plot(numberOfNeighbors, testingScoreRocAuc, 'magenta', label = 'roc auc score on testing set')
 plt.legend()
 plt.ylim(0,1)
 plt.ylabel('score')
 plt.xlabel('n-neighbor')
 plt.savefig('knn.png')
 plt.show()
-
